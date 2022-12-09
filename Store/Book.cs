364fb3708c5d5f9ac71fc5b9edef6276ed584b98
Tol-kovuy <smarty/maks13@gmail.com>
2022-12-09 18:19:53 +0200

    Add Entity Book

---
+++ Store/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store
+{
+    public class Book
+    {
+        public int Id { get; }
+        public string Title { get; }
+
+        public Book(string title)
+        {
+            Id = Id;
+            Title = title;
+        }
+    }
+}
