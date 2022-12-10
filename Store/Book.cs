1439f791a586bdec7798f495ccae5aea4090ecc2
Tol-kovuy <smarty/maks13@gmail.com>
2022-12-10 12:22:54 +0200

    Test_FindByISBN_to_branch

--- Store/Book.cs
+++ Store/Book.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Store
@@ -9,12 +10,28 @@ namespace Store
     public class Book
     {
         public int Id { get; }
+        public string Isbn { get; }
+        public string Autor { get; }
         public string Title { get; }
 
-        public Book(int id, string title)
+        public Book(int id, string isbn, string autor, string title)
         {
             Id = id;
+            Isbn = isbn;
+            Autor = autor;
             Title = title;
+        }
+
+        internal static bool IsIsbn(string s)
+        {
+            if (s == null)
+                return false;
+
+            s = s.Replace("-", "")
+                 .Replace(" ", "")
+                 .ToUpper();
+
+            return Regex.IsMatch(s, @"^ISBN\d{10}(\d{3})?$");
         }
     }
 }
